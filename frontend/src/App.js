93ca66cbc46e328cd4cd80253236dbd51b434918
shahzaibmuradani <shahzaib_muradani707@outlook.com>
2024-08-03 23:06:36 +0200

    architecture-setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,22 @@
+import React, {useState} from 'react'
 import './App.css';
 
 function App() {
+  const [state, setState] = useState("");
+
+  const onChange = (text) => {
+    setState(text)
+  }
+
+  const onClick = () => {
+    localStorage.setItem('cart', state);
+  }
+;
   return (
     <div className="App">
       <header className="App-header">
+        <input type="text" value={state} onChange={(e) => onChange(e.target.value)}/>
+        <input type="submit" onClick={() => onClick()}/>
         <p>Hello World</p>
       </header>
     </div>
